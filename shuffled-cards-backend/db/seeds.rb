0272f222b58b08a63722d0156cd5728d4a8efac2
github username <github email address>
2019-11-15 14:03:09 -0800

    Faker data

--- shuffled-cards-backend/db/seeds.rb
+++ shuffled-cards-backend/db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'Faker'
+
 User.destroy_all
 Deck.destroy_all
 Collection.destroy_all
@@ -13,14 +15,14 @@ Card.destroy_all
 
 def make_users_and_decks
     5.times do
-        @new_user = User.create(username: "Katana", avatar_picture:"yee",coins: 100)
-        Deck.create(user_id:@new_user.id)
+        @new_user = User.create(username: "#{Faker::Internet.email}", avatar_picture:"yee" ,coins: 100)
+        Deck.create(user_id: @new_user.id)
     end
 end
 
 def make_cards
     50.times do
-        Card.create(name: "bob", mana_cost: 2, health_points: 3, attack: 1, card_effect:"cool", card_description: "awesome",card_img: "coo")
+        Card.create(name: "#{Faker::Creature::Animal.name}", mana_cost: rand(1..8), health_points: rand(1..4), attack: rand(1..8), card_effect:"cool", card_description: "awesome",card_img: "coo")
     end
 end
 
