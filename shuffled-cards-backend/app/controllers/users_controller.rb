42b8bcd1b8a1f6b093ac4a9fede5667563ebd084
Rohit <rohitpratti@gmail.com>
2019-11-15 16:46:25 -0800

    user is set up

--- shuffled-cards-backend/app/controllers/users_controller.rb
+++ shuffled-cards-backend/app/controllers/users_controller.rb
@@ -1,2 +1,36 @@
 class UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+
+  def index
+       render :json => User.all
+   end
+
+   def show
+       @user = User.find(params[:id])
+       render :json => @user
+   end
+
+   def login
+       @username = params[:username].downcase
+       @user = User.where('lower(username) = ?', @username).first
+       render json: @user
+   end
+
+   def create
+       @user = User.create(user_params)
+       if @user.valid?
+           @token = encode_token(user_id: @user.id)
+           render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+       else
+           render json: {error: "Failed to create user."}, status: :not_acceptable
+       end
+   end
+
+
+  private
+
+   def user_params
+       params.require(:user).permit(:username, :password, :display_name, :avatar_picture, :coins)
+   end
+
 end
